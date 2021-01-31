ed5d0a44f0be0b710b2371c5b847ea08a1185020
Joseph Burke <joseph_burke@outlook.com>
2021-01-31 20:49:48 +0000

    Remove StrictMode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import App from "./components/App";
 storePromise.then(store => {
   ReactDOM.render(
     <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </Provider>,
     document.getElementById("root")
   );
